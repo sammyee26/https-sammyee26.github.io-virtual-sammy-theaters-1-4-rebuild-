aa47c3c4e991a5d3635ca48d5caa486b5bc68337
sammyee26 <gibsgab9@gmail.com>
2026-08-24 17:59:17 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Virtual Sammy Theaters 1.4 Rebuild
+// Never place secret API keys in this public file.
+// Set this to your secure deployed backend URL later.
+window.VST14_REBUILD_OUTPAINT_ENDPOINT = "";
